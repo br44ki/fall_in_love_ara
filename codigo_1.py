9ecec692df9dbda4ffbfc610f22c9689afd15778
Stroke_Boy <braakmguzmanmontoya@gmail.com>
2026-05-30 03:02:07 -0500

    Modificamos el mensaje

---
+++ codigo_1.py
@@ -0,0 +1,3 @@
+link="https://br44ki.github.io/fall_in_love_ara/Carta_1.html"
+link2="https://br44ki.github.io/fall_in_love_ara/Carta_2.html"
+link3="https://br44ki.github.io/fall_in_love_ara/Carta_3.html"
